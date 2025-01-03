ec18624d34c8a710699ad18a061b2db906bfd635
Владислав Яковлев <vi@Vladislavs-MacBook-Pro.local>
2025-01-03 19:05:07 +0300

    1/2 tests are passing

--- 1/99_hw/tree/main.go
+++ 1/99_hw/tree/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"os"
 	pathLib "path"
+	"strconv"
 	"strings"
 )
 
+var res string
+
 func main() {
+	// v := "xuipuzda"
+	// v2, _ := AddBytesInfoToFilename(v, "/Users/vi/personal_proj/golang_web_services_2024-04-26/1/99_hw/tree/testdata/zline/lorem/dolor.txt")
+	// fmt.Println("v", v)
+	// fmt.Println("v2", v2)
+
 	out := os.Stdout
 
 	if !(len(os.Args) == 2 || len(os.Args) == 3) {
@@ -18,29 +25,38 @@ func main() {
 	path := os.Args[1]
 	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
 	err := dirTree(out, path, printFiles)
+
 	if err != nil {
 		panic(err.Error())
 	}
 }
 
 func dirTree(writer io.Writer, path string, isPrintFiles bool) error {
-	FilePrefix := ""
 	DirPrefix := ""
 
-	_, err := processDirectory(path, isPrintFiles, FilePrefix, DirPrefix)
+	_, err := processDirectory(path, isPrintFiles, DirPrefix)
 	if err != nil {
 		return err
 	}
-	src := strings.NewReader("xui pizda ebana")
+	src := strings.NewReader(res)
 	if _, err := io.Copy(writer, src); err != nil {
 		return err
 	}
 	return nil
 }
 
-func processDirectory(RouterPath string, isPrintFiles bool, FilePrefix string, DirPrefix string) (Directory, error) {
+func processDirectory(RouterPath string, isPrintFiles bool, DirPrefix string) (Directory, error) {
 	// log.Println("PROCESSING FOLLOWING PATH:", RouterPath)
 	dirs, err := os.ReadDir(RouterPath)
+	var new_dirs []os.DirEntry
+	for _, dir := range dirs {
+		if !dir.IsDir() && !isPrintFiles {
+			continue
+		} else {
+			new_dirs = append(new_dirs, dir)
+		}
+	}
+	dirs = new_dirs
 	// log.Println("FOUNEDED FILES&FOLDERS:", dirs)
 
 	if err != nil {
@@ -52,35 +68,54 @@ func processDirectory(RouterPath string, isPrintFiles bool, FilePrefix string, D
 		if val.IsDir() {
 
 			if i == len(dirs)-1 {
-				fmt.Println(DirPrefix + "└───" + val.Name())
+				res += DirPrefix + "└───" + val.Name() + "\n"
 			} else {
-				fmt.Println(DirPrefix + "├───" + val.Name())
+				res += DirPrefix + "├───" + val.Name() + "\n"
 			}
 
 			postFix := ""
 			if i == len(dirs)-1 {
 				postFix = ""
 			} else {
-				postFix = "|"
+				postFix = "│"
 			}
-			dir, err := processDirectory(pathLib.Join(RouterPath, val.Name()), isPrintFiles, FilePrefix, DirPrefix+postFix+"\t")
+			dir, err := processDirectory(pathLib.Join(RouterPath, val.Name()), isPrintFiles, DirPrefix+postFix+"\t")
 			if err != nil {
 				return Directory{}, nil
 			}
 			d.dirs = append(d.dirs, dir)
 
 		} else if isPrintFiles {
+			filename, err := AddBytesInfoToFilename(val.Name(), pathLib.Join(RouterPath, val.Name()))
+			if err != nil {
+				return Directory{}, err
+			}
 
 			if i == len(dirs)-1 {
-				fmt.Println(DirPrefix + "└───" + val.Name())
+				res += DirPrefix + "└───" + filename + "\n"
 			} else {
-				fmt.Println(DirPrefix + "├───" + val.Name())
+				res += DirPrefix + "├───" + filename + "\n"
 			}
-			d.files = append(d.files, val.Name())
+			d.files = append(d.files, filename)
 		}
 	}
 
 	return d, nil
+}
+
+func AddBytesInfoToFilename(filename, path string) (string, error) {
+	fi, err := os.Stat(path)
+	if err != nil {
+		return "", err
+	}
+	// get the size
+	size := fi.Size()
+	if size == 0 {
+		filename = filename + " (empty)"
+	} else {
+		filename = filename + " (" + strconv.Itoa(int(size)) + "b)"
+	}
+	return filename, nil
 }
 
 type Directory struct {
