bb2fefeff5a00d171925d7997ff08c2a0f5b1094
Владислав Яковлев <vi@Vladislavs-MacBook-Pro.local>
2025-02-09 16:20:17 +0300

    adding pipeline functionality

--- 2/99_hw/signer/main.go
+++ 2/99_hw/signer/main.go
@@ -1,15 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
+// test hashing - produces correct result (no channels, no pipeline)
+// func main() {
+// 	res := SingleHash("0")
+
+// 	fmt.Println("res SingleHash", res)
+
+// 	res = MultiHash(res)
+// 	fmt.Println("res MultiHash", res)
+
+// 	res = CombineResults("0")
+// 	fmt.Println("CombineResults", res)
+// }
+
+// test execute pipeline
 func main() {
-	res := SingleHash("0")
+	jobs := []job{
+		job(func(in, out chan interface{}) {
+			out <- 1
+			out <- 2
+			out <- 3
 
-	fmt.Println("res SingleHash", res)
+			close(out)
+		}),
+		job(func(in, out chan interface{}) {
+			for val := range in {
+				fmt.Println("val", val)
 
-	res = MultiHash(res)
-	fmt.Println("res MultiHash", res)
+				out <- val
+			}
+		}),
+		// job(func(in, out chan interface{}) {
+		// 	for val := range in {
+		// 		fmt.Println("quadratic", val, val.(int)*val.(int))
+		// 		out <- val.(int) * val.(int)
+		// 	}
+		// }),
+	}
 
-	res = CombineResults("0")
-	fmt.Println("CombineResults", res)
+	ExecutePipeline(jobs...)
+
+	time.Sleep(time.Second * 5)
 }
+
+// func gen(out chan int, values ...int) {
+// 	defer close(out)
+// 	for _, v := range values {
+// 		out <- v
+// 	}
+// }
+
+// func main() {
+// 	out := make(chan int)
+// 	go gen(out, 1, 2, 3, 4)
+
+// 	for v := range out {
+// 		fmt.Println("v", v)
+// 	}
+// 	time.Sleep(time.Second * 5)
+// }
--- 2/99_hw/signer/signer.go
+++ 2/99_hw/signer/signer.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -29,14 +31,63 @@ func CombineResults(data string) string {
 	// probably this func should take several values as input
 	sh := SingleHash(data)
 	res := []string{sh, MultiHash(sh)}
-	// sorting should be done somehow
 
+	sort.Strings(res)
 	return strings.Join(res, "_")
 
-	// concat above
 }
 
-func ExecutePipeline() {
-	// who the fuck are you
+// jobs := []job{
+// 	job(func(in, out chan interface{}) {
+// 		// out <- 1
+// 		out <- 2
+// 		out <- 3
 
+// 		close(out)
+// 	}),
+// 	job(func(in, out chan interface{}) {
+// 		for val := range in {
+// 			fmt.Println("val", val)
+
+// 			out <- val
+// 		}
+// 	}),
+// 	// job(func(in, out chan interface{}) {
+// 	// 	for val := range in {
+// 	// 		fmt.Println("quadratic", val, val.(int)*val.(int))
+// 	// 		out <- val.(int) * val.(int)
+// 	// 	}
+// 	// }),
+// }
+
+func ExecutePipeline(jobs ...job) {
+
+	in := make(chan interface{})
+	// out := make(chan interface{})
+
+	j0 := jobs[0]
+	j1 := jobs[1]
+
+	o0 := executor(j0, in)
+	o1 := executor(j1, o0)
+
+	for v := range o1 {
+		fmt.Println("v", v)
+	}
+
+	// for _, j := range jobs {
+	// 	out := executor(j, in)
+
+	// 	in = out
+
+	// }
+
+}
+
+func executor(j job, in chan interface{}) chan interface{} {
+	out := make(chan interface{})
+
+	go j(in, out)
+
+	return out
 }
