35b388dfa91844259d57b7349f2a7db21578bb66
Владислав Яковлев <vi@Vladislavs-MacBook-Pro.local>
2025-02-18 16:59:06 +0300

    added parallel worker

--- 2/99_hw/signer/main.go
+++ 2/99_hw/signer/main.go
@@ -2,52 +2,51 @@ package main
 
 import (
 	"fmt"
-	"sync/atomic"
+	"strconv"
 	"time"
 )
 
-// test hashing - produces correct result (no channels, no pipeline)
-// func main() {
-// 	res := SingleHash("0")
-
-// 	fmt.Println("res SingleHash", res)
-
-// 	res = MultiHash(res)
-// 	fmt.Println("res MultiHash", res)
-
-// 	res = CombineResults("0")
-// 	fmt.Println("CombineResults", res)
-// }
-
-// test execute pipeline
 func main() {
+	in := make(chan interface{})
+	out := make(chan interface{})
 
-	var recieved uint32
-	jobs := []job{
-		job(func(in, out chan interface{}) {
-			out <- uint32(1)
-			out <- uint32(3)
-			out <- uint32(4)
-		}),
-		job(func(in, out chan interface{}) {
-			for val := range in {
-				out <- val.(uint32) * 3
-				time.Sleep(time.Millisecond * 100)
-			}
-		}),
-		job(func(in, out chan interface{}) {
-			for val := range in {
-				fmt.Println("collected", val)
-				atomic.AddUint32(&recieved, val.(uint32))
+	go func(out chan interface{}) {
+		defer close(out)
+		out <- uint32(5)
+		out <- uint32(1)
+		time.Sleep(time.Second * 3)
+		out <- uint32(7)
 
-				fmt.Println("IN GOROUTINE recieved BECOME", recieved)
-			}
-		}),
+	}(out)
+
+	in = out
+
+	out2 := make(chan interface{})
+	ParallelWorker(in, out2, DataSignerCrc32)
+
+	time.Sleep(time.Second * 10)
+}
+
+func ParallelWorker(in, out chan interface{}, baseFunc func(data string) string) {
+
+	for val := range in {
+		go func(val interface{}, out chan interface{}) {
+			defer close(out)
+			fmt.Println("val", val)
+
+			tmp := val.(uint32)
+			val2 := strconv.Itoa(int(tmp))
+
+			res := baseFunc(val2)
+
+			fmt.Println("res for val", res, val)
+			out <- res
+
+		}(val, out)
 	}
+}
 
-	ExecutePipeline(jobs...)
-
-	fmt.Println("result recieved", recieved)
-
-	time.Sleep(time.Second * 5)
+// OneInATimeWorker executes baseFunc sequentially
+func OneInATimeWorker(in, out chan interface{}, baseFunc func(data string) string) {
+	//
 }
