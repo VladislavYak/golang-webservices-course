a63c4182f9d83a8980e201cccd4386efa99d952b
Владислав Яковлев <vi@Vladislavs-MacBook-Pro.local>
2025-03-11 21:31:16 +0300

    more structure to folder

--- 2/async/workerpool/workerpool.go
+++ 2/async/workerpool/workerpool.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-const goroutinesNum = 3
+const goroutinesNum = 4
 
 func startWorker(workerNum int, in <-chan string) {
 	for input := range in {
@@ -18,8 +18,8 @@ func startWorker(workerNum int, in <-chan string) {
 }
 
 func main() {
-	runtime.GOMAXPROCS(0)               // попробуйте с 0 (все доступные) и 1
-	worketInput := make(chan string, 2) // попробуйте увеличить размер канала
+	// runtime.GOMAXPROCS(0)               // попробуйте с 0 (все доступные) и 1
+	worketInput := make(chan string) // попробуйте увеличить размер канала
 	for i := 0; i < goroutinesNum; i++ {
 		go startWorker(i, worketInput)
 	}
