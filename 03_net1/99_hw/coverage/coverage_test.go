0c8d501fefe948302cb4aa9fefe96b2940735c17
Владислав Яковлев <vi@Vladislavs-MacBook-Pro.local>
2025-04-01 18:11:37 +0300

    added test to check timeout, client.go coverage 90%

--- 03_net1/99_hw/coverage/coverage_test.go
+++ 03_net1/99_hw/coverage/coverage_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 // тут писать код тестов
@@ -129,6 +130,25 @@ func TestSearchServerBadRequestInvalidOrderBy(t *testing.T) {
 	sc := SearchClient{URL: ts.URL, AccessToken: "mytoken"}
 	_, err := sc.FindUsers(sr)
 
+	if err == nil {
+		t.Error("Supposed to get 400 error")
+	}
+
+}
+
+func TestSearchServerTimeout(t *testing.T) {
+	ts := httptest.NewServer(AuthMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		MainPage(w, r, XML_PATH)
+	})))
+
+	// https://medium.com/@jac_ln/how-to-test-real-request-timeout-in-golang-with-httptest-dbc72ea23d1a
+	http.DefaultTransport.(*http.Transport).ResponseHeaderTimeout = 1 * time.Millisecond
+
+	sr := SearchRequest{OrderBy: 1}
+
+	sc := SearchClient{URL: ts.URL, AccessToken: "mytoken"}
+	_, err := sc.FindUsers(sr)
+	fmt.Println("err", err)
 	if err == nil {
 		t.Error("Supposed to get 400 error")
 	}
